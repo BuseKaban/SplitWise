06c3b339ab5128d96c2f170de0e27339802e48c2
Buse Kaban <67025642+BuseKaban@users.noreply.github.com>
2023-05-16 01:46:06 +0300

    modal added
    modal added
    set camera

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,13 +1,54 @@
-import { IonItem, IonAvatar, IonLabel, IonContent, IonImg, IonList, IonModal, IonSearchbar, IonFab, IonFabButton, IonIcon } from '@ionic/react';
+import { IonItem, IonAvatar, IonLabel, IonContent, IonImg, IonList, IonModal, IonSearchbar, IonFab, IonFabButton, IonIcon, IonButton, IonInput } from '@ionic/react';
 import { addOutline } from 'ionicons/icons';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
+import { UserPhoto, usePhotoGallery } from '../../hooks/PhotoGallery';
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 
 
 
 
 const Modal: React.FC = () => {
+  const [photo, setPhoto] = useState<UserPhoto>();
+
+  const takePhoto = async () => {
+    const photo = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100,
+    });
+
+    const fileName = new Date().getTime() + ".jpeg";
+    const newPhoto = {
+      filepath: fileName,
+      webviewPath: photo.webPath
+    }
+    setPhoto(newPhoto);
+  };
   const modal = useRef<HTMLIonModalElement>(null);
+  const data = [
+    'Amsterdam',
+    'Buenos Aires',
+    'Cairo',
+    'Geneva',
+    'Hong Kong',
+    'Istanbul',
+    'London',
+    'Madrid',
+    'New York',
+    'Panama City',
+  ];
+
+  let [results, setResults] = useState([...data]);
+
+  const handleInput = (ev: Event) => {
+    let query = '';
+    const target = ev.target as HTMLIonSearchbarElement;
+    if (target) query = target.value!.toLowerCase();
+
+    setResults(data.filter((d) => d.toLowerCase().indexOf(query) > -1));
+  };
   return (
 
     <>
@@ -18,7 +59,24 @@ const Modal: React.FC = () => {
       </IonFab>
       <IonModal ref={modal} trigger="open-modal" initialBreakpoint={0.75} breakpoints={[0, 0.75]}>
         <IonContent className="ion-padding">
+          <IonButton onClick={takePhoto}>take photo</IonButton>
+          <IonImg src={photo?.webviewPath} />
+          <IonInput
+            id="custom-input"
+            label="Custom Counter Format"
+            labelPlacement="floating"
+            counter={true}
+            maxlength={20}
+            counterFormatter={(inputLength, maxLength) => `${maxLength - inputLength} characters remaining`}
+          ></IonInput>
 
+          <IonSearchbar showClearButton="focus" onIonInput={(ev) => handleInput(ev)}></IonSearchbar>
+
+          <IonList>
+            {results.map((result) => (
+              <IonItem>{result}</IonItem>
+            ))}
+          </IonList>
         </IonContent>
       </IonModal>
     </>
--- src/hooks/PhotoGallery.ts
+++ src/hooks/PhotoGallery.ts
@@ -6,13 +6,27 @@ import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
 import { Capacitor } from '@capacitor/core';
 
+export interface UserPhoto {
+    filepath: string;
+    webviewPath?: string;
+  }
+
 export function usePhotoGallery() {
+    const [photo, setPhoto] = useState<UserPhoto>();
+
     const takePhoto = async () => {
-      const photo = await Camera.getPhoto({
-        resultType: CameraResultType.Uri,
-        source: CameraSource.Camera,
-        quality: 100,
-      });
+        const photo = await Camera.getPhoto({
+            resultType: CameraResultType.Uri,
+            source: CameraSource.Camera,
+            quality: 100,
+        });
+
+        const fileName = new Date().getTime() + ".jpeg";
+        const newPhoto = {
+            filepath: fileName,
+            webviewPath: photo.webPath
+        }
+        setPhoto(newPhoto);
     };
   
     return {
