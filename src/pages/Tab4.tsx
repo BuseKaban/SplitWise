f7ae372d34273135976d9dceab14b893d74fa2c3
Buse Kaban <67025642+BuseKaban@users.noreply.github.com>
2023-05-21 06:52:49 +0300

    chart added

--- src/pages/Tab4.tsx
+++ src/pages/Tab4.tsx
@@ -1,21 +1,22 @@
-import { IonButton, IonContent, IonDatetime, IonHeader, IonLabel, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import { Chart as ChartJS, ArcElement, Legend, Tooltip } from 'chart.js';
 import { Chart } from 'react-chartjs-2';
-import TransactionModal from '../components/Modal/TransactionModal';
-import { GetTransactionTypes, currentUser, setCurrentUser } from '../utils/Users';
+import { GetAllTransactions, setCurrentUser } from '../utils/Users';
 import { useHistory } from 'react-router';
+import { useState, useEffect } from 'react';
 
 const Tab4: React.FC = () => {
   const history = useHistory();
   ChartJS.register(ArcElement, Tooltip, Legend);
 
+  const [chartValues, setChartValues] = useState(new Map<string, number>);
+
   const data = {
-    labels: [] as string[],
+    labels: Array.from(chartValues).map(value => value[0]),
     datasets: [
       {
         label: '# of Votes',
-        data: [6, 19, 3, 5, 2, 3],
+        data: Array.from(chartValues).map(value => value[1]),
         backgroundColor: [
           'rgba(255, 99, 132, 0.2)',
           'rgba(54, 162, 235, 0.2)',
@@ -37,10 +38,29 @@ const Tab4: React.FC = () => {
     ],
   };
 
-  GetTransactionTypes().forEach(element => {
 
-    data.labels.push(element);
-  });
+
+  useEffect(() => {
+
+  }, [])
+
+  GetAllTransactions().then(results => {
+    const typeWithValues = new Map<string, number>();
+
+    results.forEach(transaction => {
+      //   data.datasets[0].data = [transaction.amount, transaction.amount];
+      //   data.labels.push(transaction.type);
+      const value = typeWithValues.get(transaction.type);
+      if (value)
+        typeWithValues.set(transaction.type, value + transaction.amount);
+      else
+        typeWithValues.set(transaction.type, transaction.amount);
+    })
+
+    // data.datasets[0].data = [];
+    setChartValues(typeWithValues);
+  })
+
 
   function Logout(): void {
     history.push("/login")
@@ -60,7 +80,7 @@ const Tab4: React.FC = () => {
             <IonTitle size="large">Tab 4</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <Chart type='doughnut' data={data}></Chart>
+        <Chart key={"test"} id='test' type='doughnut' data={data}></Chart>
         <IonButton mode='ios' onClick={(e) => Logout()}>Default</IonButton>
       </IonContent>
     </IonPage>
